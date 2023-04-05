c18a0e77d443a95c020bbf5380ecaf0026b491ed
Olaf Draeger <olaf@lexemo.com>
2023-04-05 11:41:00 +0200

    initual push

---
+++ src/services/EventService.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'https://my-json-server.typicode.com/olaf-lexemo/data-test',
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+})
+
+export default {
+  getEvents() {
+    return apiClient.get('/events')
+  },
+  
+  getEvent(id) {
+    return apiClient.get('/events/' + id)
+  },
+
+}
